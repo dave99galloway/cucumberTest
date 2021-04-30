6ad27702c09d4fd2c134d7d7565dbfd30211562f
david galloway <david.galloway@yahoo.co.uk>
2021-04-30 16:06:25 +0100

    master - enable Masterthoughts reporting via Spacial Circumstances gradle plugin

--- src/cucumberTest/kotlin/com/github/dave99galloway/cucumbertest/example/glue/ExampleSteps.kt
+++ src/cucumberTest/kotlin/com/github/dave99galloway/cucumbertest/example/glue/ExampleSteps.kt
@@ -1,19 +1,34 @@
 package com.github.dave99galloway.cucumbertest.example.glue
 
+import io.cucumber.java.Before
+import io.cucumber.java.Scenario
 import io.cucumber.java8.En
+import java.io.File
 
 class ExampleSteps : En {
+
+    private lateinit var stepScenario: Scenario
+
+    @Before
+    fun before(scenario: Scenario) {
+        stepScenario = scenario
+    }
+
     init {
         Given("some preconditions") {
-
+            stepScenario.log("something's about to happen")
         }
 
         When("I take some action") {
-
+            stepScenario.attach(
+                File("src/cucumberTest/resources/cucumber.icon.png").readBytes(),
+                "image/png",
+                "fake screenshot"
+            )
         }
 
         Then("some result happens") {
-            throw Exception("ta da")
+            stepScenario.log("did it work?")
         }
     }
 }
