53d2de08d72a1a2f397404797800c265a4d73a8d
david galloway <david.galloway@yahoo.co.uk>
2021-04-30 15:49:24 +0100

    master - enable Masterthoughts reporting via Spacial Circumstances gradle plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,6 +6,10 @@ plugins {
 
     // Apply the java-library plugin for API and implementation separation.
     `java-library`
+
+    // https://github.com/SpacialCircumstances/gradle-cucumber-reporting
+    id("com.github.spacialcircumstances.gradle-cucumber-reporting") version "0.1.23"
+
 }
 
 repositories {
@@ -66,12 +70,13 @@ tasks.named<Wrapper>("wrapper") {
     distributionType = Wrapper.DistributionType.ALL
 }
 
+val cucumberReportsDir: String = layout.buildDirectory.dir("cucumber-reports").get().asFile.absolutePath
+val me = "cucumberTest" // alias for the "namespace" to use in the env var lookup
+
 val cucumberTest = task<JavaExec>("cucumberTest") {
     // dependsOn assemble, testClasses // fix later, for now manually call clean & build
 
     // cucumber cli options
-    val cucumberReportsDir = layout.buildDirectory.dir("cucumber-reports").get().asFile.absolutePath
-    val me = "cucumberTest" // alias for the "namespace" to use in the env var lookup
     // for this to work with the IDEA run/debug config an the EnvFile plugin, the "experimental integrations" checkbox must be set
     // if this breaks (as the warning on the checkbox implies it might do), then revert to using Dotenv as per the previous commit
     val tags: String = System.getenv("$me.tags") ?: "not @Ignore"
@@ -97,6 +102,30 @@ val cucumberTest = task<JavaExec>("cucumberTest") {
 }
 
 // tasks.check { dependsOn(integrationTest) }
+
+cucumberReports {
+    outputDir = file(cucumberReportsDir)
+    val reportMe = "$me.cucumberReports"
+    buildId = System.getenv("$reportMe.buildId") ?: System.currentTimeMillis().toString()
+    reports = files("$cucumberReportsDir/cucumber.json")
+    testTasksFinalizedByReport = false
+    runWithJenkins = System.getenv("$reportMe.runWithJenkins").toBoolean()
+    projectNameOverride = System.getenv("$reportMe.projectNameOverride")
+    //todo: enable parameterization of this path
+    val trendsPath = if (System.getenv("$reportMe.trends").toBoolean()) layout.projectDirectory.dir(".gradle")
+        .file("cucumberReports.trends.json").asFile else null
+    trends = trendsPath
+    val trendsLimitValue = System.getenv("$reportMe.trendsLimit")
+    trendsLimit = trendsLimitValue?.toInt() ?: 0
+    //  todo: enable setting these configurations via env vars (or other)
+    //  classifications: A map with <String, String> pairs that are added to the HTML report, for example os name etc.
+    //                   Use the method classification to add a single classification.
+    //                   Setting this property directly will overwrite old classifications.
+    //  excludeTags: A List<String> of regexes that will filter out tags so they are not present in the generated report.
+    //  expandAllSteps: Set this to true to make all scenarios expanded in the generated report.
+    //  notFailingStatuses: (Set<String>) Step statuses that should not be marked as failed in the report generation
+    //  directorySuffix: String. Sets a suffix for directories.
+}
 
 java {
     sourceCompatibility = JavaVersion.VERSION_1_8
